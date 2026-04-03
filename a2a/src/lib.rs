750ba86b83c6c5edf562fbce288993c4fdcd0003
Luca Muscariello <muscariello@ieee.org>
2026-04-04 00:13:42 +0200

    docs: add crate rustdoc for agntcy-a2a
    Signed-off-by: Luca Muscariello <muscariello@ieee.org>

--- a2a/src/lib.rs
+++ a2a/src/lib.rs
@@ -1,5 +1,7 @@
 // Copyright AGNTCY Contributors (https://github.com/agntcy)
 // SPDX-License-Identifier: Apache-2.0
+#![doc = include_str!("../README.md")]
+
 pub mod agent_card;
 pub mod errors;
 pub mod event;
